0cf8ec9a262c2d63e0208ad9b591fa229de21b5d
David Na <david.na@added-value.com>
2012-09-06 16:17:01 -0700

    back to test #2 (stable)

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -5,19 +5,13 @@
 // };
 
 function Game() {
-	var rolls = new Array(21);
-	var currentRoll = 0;
+	var total = 0;
 
 	this.roll = function(pinsDown) {
-		this.rolls[currentRoll] = pinsDown;
+		total += pinsDown;
 	}
 
 	this.score = function() {
-		var total = 0;
-
-		for(var i = 0; i < this.rolls.length; i++)
-			total += this.rolls[i];
-
 		return total;
 	}
 };
